73be6025906625426cde95e24f95ec7b529b81f3
advait renduchintala <advait.rs@gmail.com>
2023-12-05 16:38:34 -0600

    working with good style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,132 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import SearchBar from './SearchBar';
-// import GenreDropdown from './GenreDropdown';
-// import Login from './Login';
-// import SignUp from './Signup';
-// import Game from './GameGenre'; // Importing Game component from GameGenre.js
-// import Recommendations from './Recommendation';
-// import './App.css';
-
-// const App = () => {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const [isSigningUp, setIsSigningUp] = useState(false);
-//   const [userID, setUserID] = useState(null);
-//   const [games, setGames] = useState([]);
-//   const [username, setUsername] = useState('');
-//   const [recommendations, setRecommendations] = useState([]);
-  
-//   useEffect(() => {
-//     if (isLoggedIn && userID) {
-//         fetchRecommendations();
-//     }
-// }, [isLoggedIn, userID]);
-
-// const fetchRecommendations = () => {
-//   console.log('Fetching recommendations for userID:', userID);
-//   fetch(`/api/recommendations?userID=${userID}`)
-//       .then(response => response.json())
-//       .then(data => {
-//           console.log('Received data:', data);
-//           if (data.success) {
-//               setRecommendations(data.recommendations);
-//           } else {
-//               console.error('Failed to fetch recommendations:', data.message);
-//           }
-//       })
-//       .catch(error => console.error('Error fetching recommendations:', error));
-// };
-
-// const handleLoginSuccess = (loggedInUsername, loggedInUserID) => {
-//   console.log('Login success:', loggedInUsername, loggedInUserID);
-//   setIsLoggedIn(true);
-//   setUsername(loggedInUsername);
-//   setUserID(loggedInUserID);
-//   fetchRecommendations();
-// };
-
-//   const handleSignUpSuccess = (signedUpUsername) => {
-//     setIsLoggedIn(true);
-//     setIsSigningUp(false);
-//     setUsername(signedUpUsername);
-//   };
-
-//   const handleSearch = (term) => {
-//     fetch(`http://localhost:8080/search?term=${term}`)
-//       .then(response => response.json())
-//       .then(data => setGames(data))
-//       .catch(error => console.error('Error:', error));
-//   };
-
-//   const handleGenreSelect = (genre) => {
-//     if (!genre) {
-//       setGames([]);
-//       return;
-//     }
-//     fetch(`http://localhost:8080/games?genre=${genre}`)
-//       .then(response => response.json())
-//       .then(data => setGames(data))
-//       .catch(error => console.error('Error:', error));
-//   };
-
-//   const toggleSignUp = () => {
-//     setIsSigningUp(!isSigningUp);
-//   };
-
-//   const handleFavorite = (username, gameName) => {
-//     console.log(`Attempting to update favorite game for user ${username} with game ${gameName}`);
-//     fetch('http://localhost:8080/api/favorite', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ username, gameName }),
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log('Response from server:', data);
-//       if (data.success) {
-//         alert('Favorite game updated successfully');
-//       } else {
-//         alert('Failed to update favorite game:', data.message);
-//       }
-//     })
-//     .catch(error => {
-//       console.error('Error updating favorite game:', error);
-//     });
-//   };
-
-//   if (!isLoggedIn) {
-//     return (
-//       <div className="App">
-//         {isSigningUp ? (
-//           <SignUp onSignUpSuccess={handleSignUpSuccess} />
-//         ) : (
-//           <Login onLoginSuccess={handleLoginSuccess} />
-//         )}
-//         <button onClick={toggleSignUp}>
-//           {isSigningUp ? 'Already have an account? Log In' : "Don't have an account? Sign Up"}
-//         </button>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="background-image">
-//       <div className="App">
-//         <div>Welcome to the system, {username}!</div>
-//         <SearchBar onSearch={handleSearch} />
-//         <GenreDropdown onGenreSelect={handleGenreSelect} />
-//         {games.map(game => 
-//             <Game key={game.id} gameName={game.GameName} username={username} onFavorite={handleFavorite} />
-//         )}
-//         {isLoggedIn && <Recommendations games={recommendations} />}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import GenreDropdown from './GenreDropdown';
@@ -150,57 +21,7 @@ const App = () => {
         }
     }, [isLoggedIn, userID]);
 
-    // const fetchRecommendations = () => {
-    //     console.log('Fetching recommendations for userID:', userID);
-    //     fetch(`/api/recommendations?userID=${userID}`)
-    //     .then(response => {
-    //         if (!response.ok) {
-    //           throw new Error(`HTTP error! Status: ${response.status}`);
-    //         }
-    //           return response.json();
-    //         })
-    //         .then(data => {
-    //             console.log('Received data:', data);
-    //             if (data.success) {
-    //                 setRecommendations(data.recommendations);
-    //             } else {
-    //                 console.error('Failed to fetch recommendations:', data.message);
-    //             }
-    //         })
-    //         .catch(error => console.error('Error fetching recommendations:', error));
-    // };
-  //   const fetchRecommendations = () => {
-  //     console.log('Fetching recommendations for userID:', userID);
-  //     fetch(`http://localhost:8080/api/recs?userID=${userID}`)
-  //         .then(response => {
-  //             if (!response.ok) {
-  //                 throw new Error(`HTTP error! Status: ${response.status}`);
-  //             }
-  //             return response.text(); // Temporarily use text to see what is returned
-  //         })
-  //         .then(data => {
-  //             console.log('Received data:', data); // Check what's being returned here
-  //             // Convert data back to JSON here, after ensuring it's correct
-  //         })
-  //         .catch(error => console.error('Error fetching recommendations:', error));
-  // };
-
-//   const fetchRecommendations = () => {
-//     console.log('Fetching recommendations for userID:', userID);
-//     fetch(`http://localhost:8080/api/recs?userID=${userID}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log('Received data:', data);
-//             if (data.success) {
-//                 // Map the array of objects to an array of strings (game names)
-//                 const gameNames = data.recommendations.map(item => item.RecommendedGameName);
-//                 setRecommendations(gameNames);
-//             } else {
-//                 console.error('Failed to fetch recommendations:', data.message);
-//             }
-//         })
-//         .catch(error => console.error('Error fetching recommendations:', error));
-// };
+   
 
 const fetchRecommendations = () => {
   if (userID) { // Check if userID is not null or undefined
@@ -288,32 +109,41 @@ const fetchRecommendations = () => {
 
     return (
         <div className="background-image">
-            <div className="App">
-                {!isLoggedIn ? (
-                    <div>
-                        {isSigningUp ? (
-                            <SignUp onSignUpSuccess={handleSignUpSuccess} />
-                        ) : (
-                            <Login onLoginSuccess={handleLoginSuccess} />
-                        )}
-                        <button onClick={toggleSignUp}>
-                            {isSigningUp ? 'Already have an account? Log In' : "Don't have an account? Sign Up"}
-                        </button>
-                    </div>
+          <div className="App">
+            {!isLoggedIn ? (
+              <div className="authentication-section">
+                {isSigningUp ? (
+                  <SignUp onSignUpSuccess={handleSignUpSuccess} />
                 ) : (
-                    <div>
-                        <div>Welcome to the system, {username}!</div>
-                        <SearchBar onSearch={handleSearch} />
-                        <GenreDropdown onGenreSelect={handleGenreSelect} />
-                        {games.map(game => 
-                            <Game key={game.id} gameName={game.GameName} username={username} onFavorite={handleFavorite} />
-                        )}
-                        <Recommendations games={recommendations} />
-                    </div>
+                  <Login onLoginSuccess={handleLoginSuccess} />
                 )}
-            </div>
+                <button onClick={toggleSignUp}>
+                  {isSigningUp ? 'Already have an account? Log In' : "Don't have an account? Sign Up"}
+                </button>
+              </div>
+            ) : (
+              <div className="content-container">
+                <div className="for-you-page">
+                  <h2>For You</h2>
+                  <Recommendations games={recommendations} />
+                </div>
+                <div className="search-results">
+                    <div className="welcome-message">
+                        <h2>Welcome to the system, {username}!</h2>
+                    </div>
+                    <SearchBar onSearch={handleSearch} />
+                    <GenreDropdown onGenreSelect={handleGenreSelect} />
+                    <div className="games-list">
+                        {games.map(game => (
+                        <Game key={game.id} gameName={game.GameName} metacriticScore={game.MetacriticScore} username={username} onFavorite={handleFavorite} />
+                        ))}
+                    </div>
+                    </div>
+              </div>
+            )}
+          </div>
         </div>
-    );
-};
+      );
+    };
 
 export default App;
